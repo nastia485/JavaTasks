a7fdfbfaa54f1005c98af2c819988c005daa07bb
Nastia <62216391+nastia485@users.noreply.github.com>
2021-09-12 21:05:56 +0300

    Add files via upload

---
+++ 3. gameJava/src/company/Main.java
@@ -0,0 +1,12 @@
+
+package company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Controller controller = new Controller(new Model(), new View());
+
+        controller.processUser();
+    }
+}
